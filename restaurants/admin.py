4fffce8874f8282e0bb236ab787eb5c8b6dfd40b
jeff97830133 <jeff97830133@gmail.com>
2016-04-11 13:39:40 +0800

    20160411

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -9,7 +9,7 @@ class RestaurantAdmin(admin.ModelAdmin):
 class FoodAdmin(admin.ModelAdmin):
 	list_display = ('name', 'restaurant', 'price')	#介面中顯示name restaurant price
 	list_filter = ('is_spicy',)#增加布林值過濾器
-	fields = ('price', 'restaurant')	#鎖定選擇以外無法被編輯
+	#fields = ('price', 'restaurant')	#鎖定選擇以外無法被編輯
 	ordering = ('price',) #降冪or升冪price 正升負降
 
 admin.site.register(Restaurant, RestaurantAdmin)
--- restaurants/views.py
+++ restaurants/views.py
@@ -23,17 +23,23 @@ def comment(request,id):
 		r = Restaurant.objects.get(id=id)
 	else:
 		return HttpResponseRedirect("/restaurants_list/")
+	errors = []
 	if request.POST:
 		visitor = request.POST['visitor']
 		content = request.POST['content']
 		email = request.POST['email']
 		date_time = timezone.localtime(timezone.now()) #目前時間
-		Comment.objects.create(
-			visitor=visitor,
-			email=email,
-			content=content,
-			date_time=date_time,
-			restaurant=r
-			)
+		if any(not request.POST[k] for k in request.POST):
+			errors.append('*有空白欄位，請不要留空白')
+		if '@' not in email:
+			errors.append('* email格式不正確，請重新輸入')
+		if not errors:
+			Comment.objects.create(
+				visitor=visitor,
+				email=email,
+				content=content,
+				date_time=date_time,
+				restaurant=r
+				)
 	return render_to_response('comments.html', RequestContext(request, locals()))
 	
